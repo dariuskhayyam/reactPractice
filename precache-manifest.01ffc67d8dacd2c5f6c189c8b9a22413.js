55dbe95d3cabb239102336c7b84603504aa6a973
darius khayyam <dv.Khayyam@gmail.com>
2020-01-26 11:07:00 +0200

    Updates

---
+++ precache-manifest.01ffc67d8dacd2c5f6c189c8b9a22413.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c997b7842a8d393625eb243da462229",
+    "url": "/reactPractice/index.html"
+  },
+  {
+    "revision": "faca0eecd79feef46e02",
+    "url": "/reactPractice/static/js/2.63efa5dd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reactPractice/static/js/2.63efa5dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7798e08776552e54702d",
+    "url": "/reactPractice/static/js/main.2b6acc06.chunk.js"
+  },
+  {
+    "revision": "9d4849300bfbfe488adf",
+    "url": "/reactPractice/static/js/runtime-main.a008e18c.js"
+  }
+]);
